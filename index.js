30a17dee843a66fc5b2266e54de5226fd1a3de84
victor@victor <victor.dmaina@gmail.com>
2024-03-20 00:45:58 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob'
+function getCustomerName() {
+  return customerName;
+}
+ function upperCaseCustomerName() {
+    window.customerName === 'bob' 
+    window.customerName = window.customerName.toUpperCase();
+}
+var bestCustomer;
+function setBestCustomer() {
+ bestCustomer = 'not bob';
+}
+
+var bestCustomer;
+function overwriteBestCustomer() {
+ bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'Jackie Maribe';
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'bob'
+}
